312a883cdf48bf831f5618f178fa102d831bc513
adankitdutta <adankitdutta@gmail.com>
2021-08-16 23:29:28 +0530

    update to button displaying conditions in requestrow.js

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table, Button } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
 import Campaign from "../ethereum/campaign";
@@ -8,52 +8,84 @@ function RequestRow(props) {
   const { id, request, approversCount } = props;
   const readyToFinalize = request.approvalCount > approversCount / 2;
 
+  const [state, setState] = useState({
+    countOfApproval: request.approvalCount,
+    finalize: readyToFinalize,
+    complete: request.complete,
+  });
+
+  const [isApproving, setIsApproving] = useState(false);
+  const [isFinalizing, setIsFinalizing] = useState(false);
+
   const onApprove = async () => {
+    setIsApproving(true);
     const campaign = Campaign(props.address);
     const accounts = await web3.eth.getAccounts();
     try {
       await campaign.methods.approveRequest(props.id).send({
         from: accounts[0],
       });
+
+      //for updating the approvals count and finalize option
+      var rq = await campaign.methods.requests(props.id).call();
+      var ac = rq.approvalCount;
+      setState({
+        ...state,
+        countOfApproval: ac,
+        finalize: ac > approversCount / 2,
+      });
     } catch (err) {
       window.alert(err.message);
     }
+    setIsApproving(false);
   };
 
   const onFinalize = async () => {
+    setIsFinalizing(true);
     const campaign = Campaign(props.address);
     const accounts = await web3.eth.getAccounts();
     try {
       await campaign.methods.finalizeRequest(props.id).send({
         from: accounts[0],
       });
+
+      //for updating the finalize button and row update
+      var rq = await campaign.methods.requests(props.id).call();
+      var c = rq.complete;
+      setState({
+        ...state,
+        complete: c,
+      });
     } catch (err) {
       window.alert(err.message);
     }
+    setIsFinalizing(false);
   };
 
   return (
-    <Row
-      disabled={request.complete}
-      positive={readyToFinalize && !request.complete}
-    >
+    <Row disabled={state.complete} positive={state.finalize && !state.complete}>
       <Cell>{id}</Cell>
       <Cell>{request.description}</Cell>
       <Cell>{web3.utils.fromWei(request.value, "ether")}</Cell>
       <Cell>{request.receipent}</Cell>
       <Cell>
-        {request.approvalCount}/{approversCount}
+        {state.countOfApproval}/{approversCount}
       </Cell>
       <Cell>
-        {request.complete ? null : (
-          <Button color="green" basic onClick={onApprove}>
+        {state.complete ? null : (
+          <Button loading={isApproving} color="green" basic onClick={onApprove}>
             Approve
           </Button>
         )}
       </Cell>
       <Cell>
-        {request.complete ? null : (
-          <Button color="teal" basic onClick={onFinalize}>
+        {state.complete || !state.finalize ? null : (
+          <Button
+            loading={isFinalizing}
+            color="teal"
+            basic
+            onClick={onFinalize}
+          >
             Finalize
           </Button>
         )}
